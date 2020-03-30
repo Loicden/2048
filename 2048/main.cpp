e92f756100d04c8f13e02a933936c001b51d5e7e
JulienGuinot <45630763+Pliploop@users.noreply.github.com>
2020-03-30 14:25:46 +0200

    commit mais claqué

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    bool game=true;
+
 /*
 
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -23,6 +25,9 @@ int main(int argc, char *argv[])
     return app.exec();
 */
       Grille Grille2048 = Grille();
+      Grille2048.RandCase(false);
+      Grille2048.RandCase(false);
+      Grille2048.AfficherGrille();
 /*
       Grille2048.AfficherGrille();
       Grille2048.RandCase(false);
@@ -32,31 +37,32 @@ int main(int argc, char *argv[])
       Grille2048.RandCase();
       Grille2048.AfficherGrille();
 */
-      Grille2048.Changeval(0,0,2);
-      Grille2048.Changeval(0,3,2);
-      Grille2048.Changeval(0,2,2);
-      Grille2048.AfficherGrille();
-      /*
-      cout<<Grille2048.TestMove(0,3,0,2)<<endl;
-      cout<<Grille2048.TestMove(0,3,1,2)<<endl;
-      cout<<Grille2048.TestMove(0,3,0,0)<<endl;
-      cout<<Grille2048.TestMove(0,3,3,3)<<endl;
-      Grille2048.Move(0,3,0,2);
-      cout<<Grille2048.TestFuse(0,2,0,0)<<endl;
-      cout<<Grille2048.TestFuse(0,2,0,1)<<endl;
-      Grille2048.Move(0,2,0,3);
-      cout<<Grille2048.TestFuse(0,3,0,0)<<endl;
-      cout<<Grille2048.TestFuse(0,3,3,3)<<endl;
-      Grille2048.Fuse(0,3,0,0);
-      Grille2048.AfficherGrille();
+      //Grille2048.Changeval(0,0,2);
+      //Grille2048.Changeval(0,3,2);
+      //Grille2048.Changeval(0,2,2);
+      //Grille2048.AfficherGrille();
 
-*/
-      Grille2048.Coup(1);     //Gauche
+//      cout<<Grille2048.TestMove(0,3,0,2)<<endl;
+//      cout<<Grille2048.TestMove(0,3,1,2)<<endl;
+//      cout<<Grille2048.TestMove(0,3,0,0)<<endl;
+//      cout<<Grille2048.TestMove(0,3,3,3)<<endl;
+//      Grille2048.Move(0,3,0,2);
+//      cout<<Grille2048.TestFuse(0,2,0,0)<<endl;
+//      cout<<Grille2048.TestFuse(0,2,0,1)<<endl;
+//      Grille2048.Move(0,2,0,3);
+//      cout<<Grille2048.TestFuse(0,3,0,0)<<endl;
+//      cout<<Grille2048.TestFuse(0,3,3,3)<<endl;
+//      Grille2048.Fuse(0,3,0,0);
+//      Grille2048.AfficherGrille();
+
+      Grille2048.Coup(3);     //Gauche
       //Grille2048.Coup(2);     //Droite
       //Grille2048.Coup(3);     //Haut
       //Grille2048.Coup(4);     //Bas
-
       Grille2048.AfficherGrille();
+
+
+
 
 
 //int k;
