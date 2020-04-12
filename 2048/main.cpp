5986f9f19b17de492469884101502078f4b2789d
JulienGuinot <45630763+Pliploop@users.noreply.github.com>
2020-04-12 19:29:42 +0200

    commentaire des tests

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -11,24 +11,24 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-//    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-//    QGuiApplication app(argc, argv);
-//    QQmlApplicationEngine engine;
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
     Grille Grille2048=Grille();
     Grille2048.newGame();
     QGrille GrilleQML(Grille2048);
-    GrilleQML.GrilleG.AfficherGrille();
-    GrilleQML.Coup(2);
-    GrilleQML.GrilleG.AfficherGrille();
+//    GrilleQML.GrilleG.AfficherGrille();
+//    GrilleQML.Coup(2);
+//    GrilleQML.GrilleG.AfficherGrille();
 
-//    const QUrl url(QStringLiteral("qrc:/main.qml"));
-//    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-//                     &app, [url](QObject *obj, const QUrl &objUrl) {
-//        if (!obj && url == objUrl)
-//            QCoreApplication::exit(-1);
-//    }, Qt::QueuedConnection);
-//    engine.load(url);
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
 
-//    return app.exec()
+    return app.exec();
       return 0;
 }
